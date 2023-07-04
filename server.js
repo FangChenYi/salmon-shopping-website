ef9913d07bd1ac573b8699a264e97803cb57d6ed
x1 <fx17896@gmail.com>
2023-07-04 18:29:03 +0800

    commit

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const passport = require("passport");
 require("./config/passport")(passport);
 const Product = require("./models").product;
 const path = require("path");
-const Image = require("./models/image-model");
-const upload = require("./multer/multerMiddleware");
 const port = process.env.PORT || 8080;
 
 mongoose
@@ -46,45 +44,6 @@ app.use(
   sellerRoute
 );
 
-app.post("/upload", upload.single("image"), async (req, res) => {
-  try {
-    // 從 req.file.buffer 中取得圖片的 Buffer
-    const imageBuffer = req.file.buffer;
-    // 建立新的圖片物件
-    // const { name } = req.body;
-    const image = new Image({
-      // name, // 從請求的表單中取得圖片名稱
-      image: imageBuffer,
-    });
-
-    // 儲存圖片到資料庫
-    await image.save();
-
-    res.status(200).send("圖片上傳成功！");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("伺服器錯誤");
-  }
-});
-
-app.get("/upload", async (req, res) => {
-  try {
-    let foundProduct = await Image.find({}).exec();
-    return res.send(foundProduct);
-  } catch (e) {
-    return res.status(500).send("載入商品過程發生錯誤");
-  }
-});
-
-app.delete("/upload", async (req, res) => {
-  try {
-    let foundProduct = await Image.deleteMany({}).exec();
-    return res.send(foundProduct);
-  } catch (e) {
-    return res.status(500).send("載入商品過程發生錯誤");
-  }
-});
-
 // 查看所有商品;
 app.get("/", async (req, res) => {
   try {
@@ -92,20 +51,6 @@ app.get("/", async (req, res) => {
     return res.send(foundProduct);
   } catch (e) {
     return res.status(500).send("載入商品過程發生錯誤");
-  }
-});
-
-// 使用商品名稱搜尋商品
-app.get("/search/:title", async (req, res) => {
-  let { title } = req.params;
-  try {
-    let foundProducts = await Product.find({
-      title: { $regex: title, $options: "i" },
-    }).exec();
-    return res.send(foundProducts);
-  } catch (e) {
-    console.log(e);
-    return res.status(500).send(e);
   }
 });
 
